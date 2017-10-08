65d41a221491ed77a8bcfff54e3160c2f640d146
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-10-07 23:29:11 -0500

    Added closests sites to list

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,24 +4,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
     
     /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     *	- or -
-     * 		http://example.com/index.php/welcome/index
-     *	- or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
+     * Index Page for this controller
      */
     public function index()
     {
+        // get all sites
         $data['sites'] = $this->sites->get_all();
-        $data['location'] = get_current_location();
+        
+        // get default location from server while browser data is retrieved
+        $data['default_location'] = get_current_location();
+        
+        $data['min_distance'] = $this->config->item('min_distance');
+        $data['max_distance'] = $this->config->item('max_distance');
+        
+        $data['google_maps_api_key'] = $this->config->item('google_maps_api_key');
+        
+        // flush data to view
         $this->load->view('welcome_form', $data);
+    }
+    
+    public function map() {
+        
+        // get all sites
+        $data['sites'] = $this->sites->get_all();
+        
+        $data['google_maps_api_key'] = $this->config->item('google_maps_api_key');
+        
+        $this->load->view('show_map', $data);
     }
 }
