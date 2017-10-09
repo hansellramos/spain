5ea2ec8ead738917b5a0a98b337e483394cf40ff
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-10-08 20:32:53 -0500

    Added new users table structure

--- database-develop.sql
+++ database-develop.sql
@@ -29,7 +29,7 @@ CREATE TABLE `sites` (
   `longitude` decimal(11,8) NOT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `sites` (
 
 LOCK TABLES `sites` WRITE;
 /*!40000 ALTER TABLE `sites` DISABLE KEYS */;
-INSERT INTO `sites` VALUES (1,'la cafeteria de ARELY',6.20767130,-75.56391480,'2017-10-07 18:38:53'),(2,'Restaurante La Rue',6.20775040,-75.56394410,'2017-10-07 18:39:12'),(3,'La Rue Bistro Bar',6.20772840,-75.56386530,'2017-10-07 18:47:48'),(4,'Brasas Parrilla Bar (Llano Grande) En Viscaya El Poblado',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(5,'Coffetech',6.20855100,-75.56373650,'2017-10-07 18:47:48'),(6,'harry''s deli snacks',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(7,'Dolce Aroma Cafe',6.20833700,-75.56338840,'2017-10-07 18:47:48'),(8,'Jenos Pizza Vizcaya',6.20829250,-75.56315210,'2017-10-07 18:47:48'),(9,'Subway',6.20808800,-75.56333250,'2017-10-07 18:47:48'),(10,'Buñuelos San Lucas',6.20818140,-75.56326610,'2017-10-07 18:47:48'),(11,'Juan Valdez Cafe Vizcaya',6.20805970,-75.56326410,'2017-10-07 18:47:48');
+INSERT INTO `sites` VALUES (1,'la cafeteria de ARELY',6.20767130,-75.56391480,'2017-10-07 18:38:53'),(2,'Restaurante La Rue',6.20775040,-75.56394410,'2017-10-07 18:39:12'),(3,'La Rue Bistro Bar',6.20772840,-75.56386530,'2017-10-07 18:47:48'),(4,'Brasas Parrilla Bar (Llano Grande) En Viscaya El Poblado',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(5,'Coffetech',6.20855100,-75.56373650,'2017-10-07 18:47:48'),(6,'harry\'s deli snacks',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(7,'Dolce Aroma Cafe',6.20833700,-75.56338840,'2017-10-07 18:47:48'),(8,'Jenos Pizza Vizcaya',6.20829250,-75.56315210,'2017-10-07 18:47:48'),(9,'Subway',6.20808800,-75.56333250,'2017-10-07 18:47:48'),(10,'Buñuelos San Lucas',6.20818140,-75.56326610,'2017-10-07 18:47:48'),(11,'Juan Valdez Cafe Vizcaya',6.20805970,-75.56326410,'2017-10-07 18:47:48'),(12,'KDV',40.96575940,-5.66105850,'2017-10-08 01:15:24');
 /*!40000 ALTER TABLE `sites` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,11 +54,9 @@ CREATE TABLE `users` (
   `name` varchar(200) NOT NULL,
   `lastname` varchar(200) NOT NULL,
   `created` datetime NOT NULL,
-  `site` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_user_site_idx` (`site`),
-  CONSTRAINT `fk_user_site` FOREIGN KEY (`site`) REFERENCES `sites` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `site` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +65,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'hansel','ramos','2017-10-08 00:02:06','1'),(2,'Mayha','ramos','2017-10-08 00:03:52','2'),(3,'','','2017-10-08 00:04:26','1'),(4,'Uno','Dos','2017-10-08 20:15:28','0'),(5,'tres','cuatro','2017-10-08 20:16:30','0'),(6,'cinco','seis','2017-10-08 20:17:59','0');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -79,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-07 20:04:51
+-- Dump completed on 2017-10-08 20:32:05
