09c4f76078dce90d83feb3cb1eaa28ecba455c16
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-10-10 00:50:53 -0500

    Updated database

--- database-develop.sql
+++ database-develop.sql
@@ -58,7 +58,7 @@ CREATE TABLE `sites` (
   `longitude` decimal(11,8) NOT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `sites` (
 
 LOCK TABLES `sites` WRITE;
 /*!40000 ALTER TABLE `sites` DISABLE KEYS */;
-INSERT INTO `sites` VALUES (1,'la cafeteria de ARELY',6.20767130,-75.56391480,'2017-10-07 18:38:53'),(2,'Restaurante La Rue',6.20775040,-75.56394410,'2017-10-07 18:39:12'),(3,'La Rue Bistro Bar',6.20772840,-75.56386530,'2017-10-07 18:47:48'),(4,'Brasas Parrilla Bar (Llano Grande) En Viscaya El Poblado',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(5,'Coffetech',6.20855100,-75.56373650,'2017-10-07 18:47:48'),(6,'harry\'s deli snacks',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(7,'Dolce Aroma Cafe',6.20833700,-75.56338840,'2017-10-07 18:47:48'),(8,'Jenos Pizza Vizcaya',6.20829250,-75.56315210,'2017-10-07 18:47:48'),(9,'Subway',6.20808800,-75.56333250,'2017-10-07 18:47:48'),(10,'Buñuelos San Lucas',6.20818140,-75.56326610,'2017-10-07 18:47:48'),(11,'Juan Valdez Cafe Vizcaya',6.20805970,-75.56326410,'2017-10-07 18:47:48'),(12,'KDV',40.96575940,-5.66105850,'2017-10-08 01:15:24');
+INSERT INTO `sites` VALUES (1,'la cafeteria de ARELY',6.20767130,-75.56391480,'2017-10-07 18:38:53'),(2,'Restaurante La Rue',6.20775040,-75.56394410,'2017-10-07 18:39:12'),(3,'La Rue Bistro Bar',6.20772840,-75.56386530,'2017-10-07 18:47:48'),(4,'Brasas Parrilla Bar (Llano Grande) En Viscaya El Poblado',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(5,'Coffetech',6.20855100,-75.56373650,'2017-10-07 18:47:48'),(6,'harry\'s deli snacks',6.20860830,-75.56346220,'2017-10-07 18:47:48'),(7,'Dolce Aroma Cafe',6.20833700,-75.56338840,'2017-10-07 18:47:48'),(8,'Jenos Pizza Vizcaya',6.20829250,-75.56315210,'2017-10-07 18:47:48'),(9,'Subway',6.20808800,-75.56333250,'2017-10-07 18:47:48'),(10,'Buñuelos San Lucas',6.20818140,-75.56326610,'2017-10-07 18:47:48'),(11,'Juan Valdez Cafe Vizcaya',6.20805970,-75.56326410,'2017-10-07 18:47:48'),(12,'KDV',40.96575940,-5.66105850,'2017-10-08 01:15:24'),(13,'Gran Hotel Corona Sol',40.96894870,-5.67061040,'2017-10-10 00:49:59');
 /*!40000 ALTER TABLE `sites` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +89,7 @@ CREATE TABLE `tracks` (
   `exit_site` varchar(200) DEFAULT NULL,
   `exit_datetime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +98,7 @@ CREATE TABLE `tracks` (
 
 LOCK TABLES `tracks` WRITE;
 /*!40000 ALTER TABLE `tracks` DISABLE KEYS */;
-INSERT INTO `tracks` VALUES (1,'hansel','ramos','1','2017-10-08 00:02:06',NULL,NULL,NULL,NULL),(2,'Mayha','ramos','2','2017-10-08 00:03:52',NULL,NULL,NULL,NULL),(4,'Uno','Dos','0','2017-10-08 20:15:28',NULL,NULL,NULL,NULL),(5,'tres','cuatro','0','2017-10-08 20:16:30',NULL,NULL,NULL,NULL),(6,'cinco','seis','0','2017-10-08 20:17:59',NULL,NULL,NULL,NULL),(7,'New','Site','la cafeteria de ARELY','2017-10-08 22:40:00',NULL,NULL,NULL,NULL),(8,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 13:06:43',NULL,NULL,NULL,NULL),(9,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 17:45:41',NULL,NULL,NULL,NULL),(10,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 17:53:40',NULL,NULL,NULL,NULL),(11,'User','Admin','Subway','2017-10-09 17:54:03',NULL,NULL,NULL,NULL),(12,'User','Admin','Buñuelos San Lucas','2017-10-09 17:54:57',NULL,NULL,NULL,NULL),(13,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 17:57:34',NULL,NULL,NULL,NULL),(14,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 18:04:53',NULL,NULL,NULL,NULL);
+INSERT INTO `tracks` VALUES (1,'hansel','ramos','1','2017-10-08 00:02:06',NULL,NULL,NULL,NULL),(2,'Mayha','ramos','2','2017-10-08 00:03:52',NULL,NULL,NULL,NULL),(4,'Uno','Dos','0','2017-10-08 20:15:28',NULL,NULL,NULL,NULL),(5,'tres','cuatro','0','2017-10-08 20:16:30',NULL,NULL,NULL,NULL),(6,'cinco','seis','0','2017-10-08 20:17:59',NULL,NULL,NULL,NULL),(7,'New','Site','la cafeteria de ARELY','2017-10-08 22:40:00',NULL,NULL,NULL,NULL),(8,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 13:06:43',NULL,NULL,'la cafeteria de ARELY','2017-10-09 22:46:06'),(9,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 17:45:41',NULL,NULL,'la cafeteria de ARELY','2017-10-09 22:46:00'),(10,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 17:53:40',NULL,NULL,'la cafeteria de ARELY','2017-10-09 22:45:26'),(11,'User','Admin','Subway','2017-10-09 17:54:03','la cafeteria de ARELY','2017-10-09 22:45:11','la cafeteria de ARELY','2017-10-09 22:45:17'),(12,'User','Admin','Buñuelos San Lucas','2017-10-09 17:54:57','la cafeteria de ARELY','2017-10-09 22:32:33','la cafeteria de ARELY','2017-10-09 22:45:02'),(13,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 17:57:34','Cafetería','2017-05-29 23:00:01','la cafeteria de ARELY','2017-10-09 22:44:55'),(14,'User','Admin','Juan Valdez Cafe Vizcaya','2017-10-09 18:04:53','la cafeteria de ARELY','2017-10-09 22:36:45','Restaurante La Rue','2017-10-09 22:37:04'),(15,'User','Admin','la cafeteria de ARELY','2017-10-09 22:46:27',NULL,NULL,'La Rue Bistro Bar','2017-10-09 22:46:34'),(16,'User','Admin','la cafeteria de ARELY','2017-10-10 00:38:18',NULL,NULL,'la cafeteria de ARELY','2017-10-10 00:38:32'),(17,'User','Admin','La Rue Bistro Bar','2017-10-10 00:38:40','la cafeteria de ARELY','2017-10-10 00:38:47','La Rue Bistro Bar','2017-10-10 00:38:55');
 /*!40000 ALTER TABLE `tracks` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -111,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-09 18:11:05
+-- Dump completed on 2017-10-10  0:50:22
